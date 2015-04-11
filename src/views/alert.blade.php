b888757ea67cc084f65d5a99d32aa3d08fca6c40
Hieu Le Trung <letrunghieu.cse09@gmail.com>
2015-04-11 09:16:18 +0000

    Update view not escape

--- src/views/alert.blade.php
+++ src/views/alert.blade.php
@@ -1,12 +1,12 @@
 <div class="alert alert-{{ $type }} alert-dismissable">
     <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
     <h4>
-        {{ $icon }}
-        {{ trans("alert::alert.title.{$type}") }}
+        {!! $icon !!}
+        {!! trans("alert::alert.title.{$type}") !!}
     </h4>
     @foreach($messages as $message)
     <p>
-        {{ $message }}
+        {!! $message !!}
     </p>
     @endforeach
 </div>
